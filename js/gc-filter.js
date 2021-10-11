2ec28b473bece791feda012baade920e7cc11eea
Johannes Sommer <jsommer@geocledian.com>
2021-10-11 11:19:45 +0200

    added planting / harvest date filter

--- js/gc-filter.js
+++ js/gc-filter.js
@@ -14,7 +14,9 @@ const gcFilterLocales = {
       "crop": "Crop",
       "entity": "Entity",
       "name": "Name",
-      "promotion": "Promotion"
+      "promotion": "Promotion",
+      "planting": "Planting date",
+      "harvest": "Harvest date",
     },
     "buttons": { 
       "applyFilter": {
@@ -31,7 +33,9 @@ const gcFilterLocales = {
       "crop": "Fruchtart",
       "entity": "Entität",
       "name": "Name",
-      "promotion": "Demo"
+      "promotion": "Demo",
+      "planting": "Pflanzdatum",
+      "harvest": "Erntedatum",
     },
     "buttons": { 
       "applyFilter": {
@@ -85,7 +89,7 @@ Vue.component('gc-filter', {
     },
     gcAvailableFields: {
       type: String,
-      default: 'crop,name,entity,promotion'
+      default: 'crop,name,entity,planting,harvest'
     },
     gcAvailableOptions: {
       type: String,
@@ -93,7 +97,7 @@ Vue.component('gc-filter', {
     },
     gcWidgetCollapsed: {
       type: Boolean,
-      default: true // or false
+      default: false // or false
     },
     gcLanguage: {
       type: String,
@@ -102,47 +106,59 @@ Vue.component('gc-filter', {
   },
   template: `<div :id="this.gcWidgetId" class="is-inline gc-filter">
 
-                <p class="gc-options-title is-size-6 is-inline-block is-orange" 
-                  style="cursor: pointer; margin-bottom: 1em;"    
+                <p :class="['gc-options-title', 'is-size-6', gcWidgetCollapsed ? 'gc-is-tertiary' : 'gc-is-primary']" 
+                  style="cursor: pointer; margin-bottom: 0.5em;"    
                   v-on:click="toggleFilter" 
                   v-show="availableOptions.includes('widgetTitle')">
                     <!--i class="fas fa-filter fa-sm"></i --> {{ $t('options.title') }}
-                    <i :class="[gcWidgetCollapsed ? '': 'is-active', 'fas', 'fa-angle-down', 'fa-sm']"></i>
+                    <i :class="[!gcWidgetCollapsed ? '': 'is-active', 'fas', 'fa-angle-down', 'fa-sm']"></i>
                 </p>
 
                 <!-- filter container -->
-                <div :class="[gcWidgetCollapsed ? '': 'is-hidden']" style="width: 100%;">
+                <div :class="[!gcWidgetCollapsed ? '': 'is-hidden']" style="width: 100%;">
                   <div :id="this.gcWidgetId + 'div'" :class="layoutCSSMap['alignment'][gcLayout]">
-                    <div class="gc-filter-field" v-show="availableFields.includes('crop')">
-                      <label class="label is-small is-grey">{{$t('fields.crop')}}</label>
+                    <div class="gc-field" v-show="availableFields.includes('crop')">
+                      <label class="label is-small gc-is-tertiary">{{$t('fields.crop')}}</label>
                       <div class="control">
                         <input type="text" class="input is-small" :placeholder="$t('fields.crop')" v-model="crop">
                       </div>
                     </div>
-                    <div class="gc-filter-field" v-show="availableFields.includes('entity')">
-                      <label class="label is-small is-grey">{{$t('fields.entity')}}</label>
+                    <div class="gc-field" v-show="availableFields.includes('entity')">
+                      <label class="label is-small gc-is-tertiary">{{$t('fields.entity')}}</label>
                       <div class="control">
                         <input type="text" class="input is-small" :placeholder="$t('fields.entity')" v-model="entity">
                       </div>
                     </div>
-                    <div class="gc-filter-field" v-show="availableFields.includes('name')">
-                      <label class="label is-small is-grey">{{$t('fields.name')}}</label>
+                    <div class="gc-field" v-show="availableFields.includes('name')">
+                      <label class="label is-small gc-is-tertiary">{{$t('fields.name')}}</label>
                       <div class="control">
                         <input type="text" class="input is-small" :placeholder="$t('fields.name')" v-model="name">
                       </div>
                     </div>
-                    <div class="gc-filter-field" v-show="availableFields.includes('promotion')">
-                      <label class="label is-small is-grey">{{$t('fields.promotion')}}</label>
+                    <div class="gc-field" v-show="availableFields.includes('planting')">
+                      <label class="label is-small gc-is-tertiary">{{$t('fields.planting')}}</label>
+                      <div class="control">
+                        <input type="text" class="input is-small" :placeholder="$t('fields.planting')" v-model="planting">
+                      </div>
+                    </div>
+                    <div class="gc-field" v-show="availableFields.includes('harvest')">
+                      <label class="label is-small gc-is-tertiary">{{$t('fields.harvest')}}</label>
+                      <div class="control">
+                        <input type="text" class="input is-small" :placeholder="$t('fields.harvest')" v-model="harvest">
+                      </div>
+                    </div>
+                    <div class="gc-field" v-show="availableFields.includes('promotion')">
+                      <label class="label is-small gc-is-tertiary">{{$t('fields.promotion')}}</label>
                       <div class="control">
                         <input type="checkbox" class="content" v-model="promotion">
                       </div>
                     </div>
-                    <div class="gc-filter-field">
+                    <div class="gc-field">
                       <div class="control" :style="gcLayout === 'vertical' ? 'padding-bottom: 1.2em;': 'margin-top: 1.2em;'">
-                        <button :id="this.gcWidgetId + '_btnApplyFilter'" class="button is-small is-light is-orange" v-on:click="applyFilter">
+                        <button :id="this.gcWidgetId + '_btnApplyFilter'" class="button is-small is-light gc-is-primary" v-on:click="applyFilter">
                             <i class="fas fa-filter fa-sm"></i><span class="content">{{$t('buttons.applyFilter.title')}}</span>
                         </button>
-                        <button :id="this.gcWidgetId + '_btnRemoveFilter'" class="button is-small is-light is-orange" v-on:click="removeFilter">
+                        <button :id="this.gcWidgetId + '_btnRemoveFilter'" class="button is-small is-light gc-is-primary" v-on:click="removeFilter">
                             <i class="fas fa-times-circle fa-sm"></i><span class="content">{{$t('buttons.removeFilter.title')}}</span>
                         </button>
                       </div>
@@ -153,16 +169,16 @@ Vue.component('gc-filter', {
                 <!-- pagination -->
                 <!-- div class="field is-centered" style="padding-top: 10px;">
                   <nav class="pagination is-small is-centered" role="navigation" aria-label="pagination">
-                    <button class="button pagination-previous is-light is-orange" v-on:click="setParcelPageStart();" title="Start">
+                    <button class="button pagination-previous is-light gc-is-primary" v-on:click="setParcelPageStart();" title="Start">
                       <i class="fas fa-fast-backward"></i>
                     </button>
-                    <button id="btnPagePrev" class="button pagination-previous is-light is-orange" v-on:click="setParcelPageOffset(-pagingStep);" title="Previous 250">
+                    <button id="btnPagePrev" class="button pagination-previous is-light gc-is-primary" v-on:click="setParcelPageOffset(-pagingStep);" title="Previous 250">
                       <i class="fas fa-step-backward"></i>
                     </button>
-                    <button id="btnPageNext" class="button pagination-next is-light is-orange" v-on:click="setParcelPageOffset(pagingStep);" title="Next 250">
+                    <button id="btnPageNext" class="button pagination-next is-light gc-is-primary" v-on:click="setParcelPageOffset(pagingStep);" title="Next 250">
                       <i class="fas fa-step-forward"></i>
                     </button>
-                    <button class="button pagination-next is-light is-orange" v-on:click="setParcelPageEnd();" title="End">
+                    <button class="button pagination-next is-light gc-is-primary" v-on:click="setParcelPageEnd();" title="End">
                       <i class="fas fa-fast-forward"></i>
                     </button>
                     <ul v-if="(total_parcel_count - offset) < pagingStep" id="parcel_paging" 
@@ -190,11 +206,12 @@ Vue.component('gc-filter', {
     return {
         parcels: [],
         total_parcel_count: 0,
-        pagingStep: 250,
         promotion: undefined,
         crop: "",
         entity: "",
         name: "",
+        planting: "",
+        harvest: "",
         currentParcel: undefined,
         selectedParcelId: -1,
         layoutCSSMap: { "alignment": {"vertical": "is-inline-block", "horizontal": "is-flex" }},
@@ -239,38 +256,67 @@ Vue.component('gc-filter', {
           return this.gcApiSecure;
       }
     },
-    filterString: {
-        get: function () {
-          // TODO offset + limit + paging
-            let filterStr = "&crop="+this.crop +
-                            "&entity="+ this.entity+
-                            "&name="+ this.name;
-            if (this.promotion) {
-                filterStr += "&promotion="+ JSON.parse(this.promotion);
-            }
-            return filterStr;
+    apiMajorVersion: {
+      get () {
+        if (this.apiBaseUrl === "/agknow/api/v3") {
+          return 3
         }
-    },
-    parcelIds: {
-      get: function () { 
-        if (this.parcels.length > 0) {
-          return this.parcels.map(p => p.parcel_id); 
+        if (this.apiBaseUrl === "/agknow/api/v4") {
+          return 4
         }
-        else {
-            return [];
-        }
-      },
-    },
-    limit: {
-      get: function() {
-        // will always reflect prop's value 
-        return this.gcLimit;
-      },
-      set: function (newValue) {       
-        //notify root - through props it will change this.gcLimit
-        this.$root.$emit('limitChange', newValue);
       }
     },
+    filterString: {
+      get: function () {
+        // TODO offset + limit + paging
+        let filterStr = "&crop="+this.crop +
+                        "&entity="+ this.entity+
+                        "&name="+ this.name;
+        if (this.apiMajorVersion === 3) {
+          if (this.promotion) {
+            filterStr += "&promotion="+ JSON.parse(this.promotion);
+          }
+        }
+        if (this.apiMajorVersion === 4) {
+          if (this.promotion) {
+            filterStr += "&extended="+ JSON.parse(this.promotion);
+          }
+          if (this.planting.length > 0) {
+            filterStr += "&planting="+ this.planting;
+          }
+          if (this.harvest.length > 0) {
+            filterStr += "&harvest="+ this.harvest;
+          }
+        }
+        return filterStr;
+      }
+    },
+    // parcelIds: {
+    //   get: function () { 
+    //     if (this.parcels.length > 0) {
+    //       return this.parcels.map(p => p.parcel_id); 
+    //     }
+    //     else {
+    //         return [];
+    //     }
+    //   },
+    // },
+    // limit: {
+    //   get: function() {
+    //     // will always reflect prop's value 
+    //     return this.gcLimit;
+    //   },
+    //   set: function (newValue) {       
+    //     //notify root - through props it will change this.gcLimit
+    //     this.$root.$emit('limitChange', newValue);
+    //   }
+    // },
+    // pagingStep: {
+    //   get: function() {
+    //     // will always reflect prop's value 
+    //     return this.limit;
+    //   },
+    // },
     offset: {
       get: function() {
         // will always reflect prop's value 
@@ -299,38 +345,35 @@ Vue.component('gc-filter', {
     }
   },
   watch: {
-      parcelIds: function(newValue, oldValue) {
-        //propagate local parcel ids to root instance
-        //this.$root.selectedParcelIds = this.parcelIds;
-      },
-      parcels: function(newValue, oldValue) {
-        console.debug("parcels changed!");
-        //console.debug(oldValue);
-        //console.debug(newValue);
+      // parcelIds: function(newValue, oldValue) {
+      //   //propagate local parcel ids to root instance
+      //   //this.$root.selectedParcelIds = this.parcelIds;
+      // },
+      // parcels: function(newValue, oldValue) {
+      //   console.debug("parcels changed!");
+      //   //console.debug(oldValue);
+      //   //console.debug(newValue);
 
-        this.$root.$emit('parcelsChange', newValue);
+      //   this.$root.$emit('parcelsChange', newValue);
 
-        // set first parcel as current parcel
-        this.currentParcel = this.parcels[0];
+      //   // set first parcel as current parcel
+      //   this.currentParcel = this.parcels[0];
         
-      },
+      // },
       filterString: function(newValue, oldValue) {
-        console.debug("filterString changed!");
-        //console.debug(oldValue);
-        //console.debug(newValue);
-      },
-      offset: function (newValue, oldValue) {
+        
+        console.debug("gc-filter - filterString");
 
-        console.debug("event - offsetChange");
-
-        this.$root.$emit('offsetChange',this.offset);
-
-        // trigger per change of filter to other listening components
-        this.applyFilter();
+        // would fire on any keyup event
+        // this.$root.$emit('filterStringChange', newValue);
       },
-      selectedParcelId: function (newValue, oldValue) {
-        this.$root.$emit('selectedParcelIdChange', newValue);
-      },
+      // offset: function (newValue, oldValue) {
+
+      //   console.debug("gc-filter - offsetChange");
+
+      //   // trigger per change of filter to other listening components
+      //   // this.applyFilter();
+      // },
       currentLanguage(newValue, oldValue) {
         this.changeLanguage();
       }
@@ -406,7 +449,7 @@ Vue.component('gc-filter', {
     getAllParcels: function (offset, filterString) {
 
         //download in chunks of n parcels
-        let limit = this.pagingStep;
+        let limit = this.limit;
 
         const endpoint = "/parcels";
         let params = "&limit=" + limit; //set limit to maximum (default 1000)
@@ -457,7 +500,7 @@ Vue.component('gc-filter', {
         console.debug("applyFilter()");
         document.getElementById(this.gcWidgetId + "_btnApplyFilter").classList.add("is-active");
        
-        this.getParcelTotalCount(this.filterString);
+        // this.getParcelTotalCount(this.filterString);
 
         // update the paging & data
         this.$root.$emit('filterStringChange', this.filterString);
@@ -469,8 +512,9 @@ Vue.component('gc-filter', {
         this.entity = "";
         this.name = "";
         this.promotion = undefined;
+        this.offset = 0;
 
-        this.pagingStep = 250;
+        this.pagingStep = this.limit;
 
         this.applyFilter();
 
